e1d970c99993f21d27126c605f57b732a31b625f
victus-owner <ashishprajapati0202@gmail.com>
2026-04-03 22:29:51 +0530

    login and regitration

---
+++ server/server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+// app.post("/api/auth/register", (req, res) => {
+//   console.log("Request received");
+//   res.send("Working");
+// });
+
+app.use("/api/auth", authRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+connectDB();
+app.use("/api/auth", authRoutes);
